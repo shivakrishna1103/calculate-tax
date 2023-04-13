212f0a3c1ce0fea2235d1de06252cfa25a51ac2e
shivakrishna <pullurishivakrishna1103@gmail.com>
2023-04-13 16:07:14 +0530

    challenge has been completed

--- src/components/IncomeTaxForm.js
+++ src/components/IncomeTaxForm.js
@@ -5,50 +5,48 @@ const IncomeTaxForm = () => {
   const [email, setEmail] = useState(" ");
   const [income, setIncome] = useState("");
 
-  
-   const handleSubmit= (event)=>{
-    event.preventDefault();
-   }
+  const calculateIncomeTax = (name, email, income) => {
+    let taxAmount;
+    if (income <= 250000) {
+      taxAmount = 0;
+    } else if (income > 250000 && income <= 500000) {
+      taxAmount = 12500;
+    } else if (income > 500001 && income <= 750000) {
+      taxAmount = 25000;
+    } else if (income > 750000 && income <= 1000000) {
+      taxAmount = 37500;
+    } else if (income >1000000 && income < 1250000) {
+      taxAmount = 50000;
+    } else if (income > 1250000 && income < 1500000) {
+      taxAmount = 62500;
+    } else if (1500000) {
+      taxAmount = 0;
+    }
 
-  
-     // call function to calculate income tax based on the input values
 
-     const taxAmount = calculateIncomeTax(name,email,income);
+    return taxAmount;
+  };
+  const handleSubmit = (event) => {
+     event.preventDefault();
 
-      const calculateIncomeTax= (name ,email,income ) =>{
-        
-        let taxAmount;
-        if(income <= 250000){
-            taxAmount = income*0;
-        } else if(income>= 250001 && income <= 500000)
-
-        {
-            taxAmount=250001+(income-250001) *5;
-        }else if(income >=500001 && income <=750000) {
-            taxAmount=15000 +(income-100000) * 0.3;
-        }
-        return taxAmount;
-      }
-      
-  
-
-     // Display the calculated tax amount 
-
-     alert(`your income tax is ${taxAmount}`)
-
- 
+    // call function to calculate income tax based on the input values
+    const taxAmount = calculateIncomeTax(name, email, income);
+    console.log(taxAmount);
+  };
 
   return (
     <div className="tax">
       <div className="tax_Form">
-        <form onSubmit={handleSubmit()}>
+        <form >
           <label name="name">Employee Name :</label>
           <input
             type="text"
             id="name"
             name="name"
             vlaue={name}
-            onChange={(event)=>{setName(event.target.value)}}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
           ></input>
           <label name="email">EmailID : </label>
           <input
@@ -56,16 +54,24 @@ const IncomeTaxForm = () => {
             id="email"
             name="name"
             value={email}
-            onChange={(event)=>{setEmail(event.target.value)}}
+            onChange={(event) => {
+              setEmail(event.target.value);
+            }}
             required
           ></input>
           <label name="total">Get Total Tax :</label>
-          <input type="text" name="income" onChange={(event)=>{
-           setIncome( event.target.value)
-          }}></input>
+          <input
+            type="text"
+            name="income"
+            onChange={(event) => {
+              setIncome(event.target.value);
+            }}
+          ></input>
 
           <div className="btn">
-               <button type="submit"  >Tax</button>
+            <button type="submit" onClick={handleSubmit}>
+              Tax
+            </button>
           </div>
         </form>
       </div>
